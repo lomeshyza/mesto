bf630f1c22a2eb39c80bbb86dfc4e407d40be998
Nadya Gallyamova <lomeshyza@yandex.ru>
2023-03-15 02:57:42 +0300

    feature: Подключен webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import {Card} from '../scripts/Card.js';
-import {FormValidator} from '../scripts/FormValidator.js';
+import {Card} from '../components/Card.js';
+import {FormValidator} from '../components/FormValidator.js';
 import {popupElementAdd,profileName,profileAbout, placeInput, imageInput, nameInput, jobInput, formEditElement, formAddElement, profileOpenButton,cardAddButton, popupElementEdit, popupElementImage} from '../utils/constants.js';
 import {Section} from '../components/Section.js';
 import {PopupWithImage} from '../components/PopupWithImage.js';
 import {PopupWithForm} from '../components/PopupWithForm.js';
-
+import '../pages/index.css'
 //Функция открытия изображения
 
 export const handleCardClick = (name, link) =>{
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
 module.exports = {
-entry: { main: './src/index.js' },
+entry: { main: './src/pages/index.js' },
 output: {
-       path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-        publicPath: ''
-  }
-}
+  filename: 'main.[hash].js',
+  path: path.resolve(__dirname, 'build'),
+},
+mode: 'development',
+devServer:{
+  static:{
+    directory: path.resolve(__dirname, 'build'),
+  },
+  open: true,
+},
+module:{
+  rules:[
+    {
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules'
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/,
+      type: 'asset/resource',
+      generator:{
+        filename: 'images/[name].[hash][ext]'
+      }
+    },
+    {
+    test: /\.(woff|woff2|eof|ttf|otf)$/,
+    type: 'asset/resource',
+    generator:{
+      filename: 'fonts/[name].[hash][ext]'
+      }
+    },
+    {
+      test: /\.css$/,
+      use:[
+        MiniCssExtractPlugin.loader,
+        {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+        }
+        },
+        'postcss-loader'
+      ],
+    }
+  ],
+},
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+  new MiniCssExtractPlugin(),
+  new CleanWebpackPlugin(),
+],
+};
 
 // module.exports — это синтаксис экспорта в Node.js
